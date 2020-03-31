2c8e0e0f2e948232ab30020252869a2d5e6a6cfa
Marcelle Vargas <marcelle@stbr.com.br>
2020-03-31 10:04:12 -0300

    aula 1 e aula 2

---
+++ alurabank/app/js/app.js
@@ -0,0 +1,17 @@
+//Instanciando a classe NegociacaoController
+const controller = new NegociacaoController();
+/**
+ * Utilizamos o .querySelector() para pegar o id que
+ * corresponde ao formulario.
+ * Depois usamos a função .addEventListener()
+ * que recebe o evento submit e envia para
+ * o evento "adiciona" do  controller
+ * (que foi declarado acima como NegociacaoController)
+ */
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
+/**
+ * Eu faço um bind para o controller para acessar o evento
+ * adiciona da classe NegociacaoController
+ */ 
