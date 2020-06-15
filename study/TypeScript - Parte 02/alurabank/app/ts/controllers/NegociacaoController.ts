7b56a479dc7b73e329ff8cf75e34bb5edfdce583
Marcelle Vargas <marcelle@stbr.com.br>
2020-06-15 09:59:56 -0300

    Reorganizando o repositório

---
+++ study/TypeScript - Parte 02/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,62 @@
+import { NegociacoesView,MensagemView } from '../views/index';
+import { Negociacoes,Negociacao } from '../models/index';
+
+export class NegociacaoController {
+
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
+
+    constructor() {
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
+        
+    }
+
+    adiciona(event: Event) {
+        const t1 = performance.now();
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            new Date(this._inputData.val().replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
+        );
+
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return 
+        }
+
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método adiciona(): ${(t2 - t1)/1000} segundos`);
+    }
+
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado
+}
